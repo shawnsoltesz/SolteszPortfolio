e6d9bb4a60ed27c7689812bf05412ea89d274482
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-10-13 17:00:35 -0400

    updates to CSS and removing test logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,58 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import logo from './images/soltesz_logo.svg'
 
 export function App() {
   return (
-    <body>
+    <div>
       <header>
         <div className="nav">
           <ul className="nav-left">
             <li>
-              <Link to="#">
-                <img
-                  src={logo}
-                  alt="Shawn Soltesz logo"
-                  width="200"
-                  height="45"
-                />
-              </Link>
+              <img
+                src={logo}
+                alt="Shawn Soltesz logo"
+                width="200"
+                height="45"
+              />
             </li>
           </ul>
 
           <ul className="nav-right">
             <li>
-              <Link to="#">
-                <i className="breadcrumb-icon fas fa-home"></i>
-              </Link>
+              <i className="breadcrumb-icon fas fa-home"></i>&nbsp;Home
             </li>
             <li>
-              <Link to="#">
-                <i className="breadcrumb-icon fas fa-info-circle"></i>
-              </Link>
+              <i className="breadcrumb-icon fas fa-info-circle"></i>&nbsp;About
             </li>
             <li>
-              <Link to="#">
-                <i className="breadcrumb-icon fas fa-search"></i>
-              </Link>
+              <i className="fas fa-briefcase"></i>&nbsp;Portfolio
+            </li>
+            <li>
+              <i className="fas fa-mail-bulk"></i>&nbsp;Contact
             </li>
           </ul>
         </div>
       </header>
       <main>
-        <div>
-          <h1>Hello and Welcome</h1>
-        </div>
+        <h1>Hello and Welcome</h1>
+
+        <h2>About</h2>
+
+        <h2>Portfolio</h2>
+
+        <h2>Contact</h2>
       </main>
-
       <footer>
-        <div className="footer-left">
-          <ul>
-            <li>
-              <a href="http://www.facebook.com/stpetepet">
-                <i className="facebook fab fa-facebook-square"></i>
-              </a>
-            </li>
-            <li>
-              <a href="http://www.instagram.com/stpetepet">
-                <i className="instagram fab fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="footer-right">
+        <div className="footer">
           <p>
             Built with <i className="heart fa fa-heart"></i> in St Petersburg,
             Florida. <i className="paw fas fa-paw"></i>
+            &nbsp;&#169;2021 Shawn Soltesz
           </p>
-          <p>&#169;2021 StPete.pet - Privacy Policy - Terms of Service</p>
         </div>
       </footer>
-    </body>
+    </div>
   )
 }
