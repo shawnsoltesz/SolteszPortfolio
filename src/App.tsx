051e757feb0a03b23000e4e15e696a4e0e2cb88a
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-10-20 11:11:53 -0400

    updated favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
-// import { Link } from 'react-router-dom'
 
 import logo from './images/shawnsoltesz_sm_logo.png'
 import headshot from './images/shawn_soltesz_headshot.jpg'
