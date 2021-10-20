a09903d9585a752444cb8d85fa7469aab699cf54
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-10-20 10:57:30 -0400

    changed the order of the contact buttons

--- src/App.tsx
+++ src/App.tsx
@@ -466,6 +466,16 @@ export function App() {
           <h2 className="section-header">Contact</h2>
           <ul>
             <li>
+              <a href="tel:410-802-6550">
+                <i className="phone fas fa-phone-alt"></i>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:shawnjsoltesz@gmail.com">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </li>
+            <li>
               <a href="https://github.com/shawnsoltesz" target="_blank">
                 <i className="github fab fa-github-square"></i>
               </a>
@@ -476,16 +486,6 @@ export function App() {
                 target="_blank"
               >
                 <i className="linkedin fab fa-linkedin"></i>
-              </a>
-            </li>
-            <li>
-              <a href="mailto:shawnjsoltesz@gmail.com">
-                <i className="fas fa-envelope"></i>
-              </a>
-            </li>
-            <li>
-              <a href="tel:410-802-6550">
-                <i className="phone fas fa-phone-alt"></i>
               </a>
             </li>
           </ul>
