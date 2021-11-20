f7ed2fb696cd65faa71fdac1c5562f54c10a200b
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-11-19 21:33:15 -0500

    troubleshooting errors on App

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-export function Footer() {
+
+export function Footer(props: any) {
   return (
+    // <footer>
+    //   <div className="footer">
+    //     <p>
+    //       Built with <i className="heart fa fa-heart"></i> in St Petersburg,
+    //       Florida&nbsp;|&nbsp;©2021 Shawn Soltesz
+    //     </p>
+    //   </div>
+    // </footer>
+
     <footer>
       <div className="footer">
         <p>
-          Built with <i className="heart fa fa-heart"></i> in St Petersburg,
-          Florida&nbsp;|&nbsp;©2021 Shawn Soltesz
+          Built with <i className="heart fa fa-heart"></i> in&nbsp;
+          {props.data ? props.data.location : 'Loading'}&nbsp;|&nbsp;©
+          {props.data ? props.data.year : 'Loading'}&nbsp; Shawn Soltesz
         </p>
       </div>
     </footer>
