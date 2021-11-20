bedc199eb21433ff9d66154aa3982f4d8b2705b1
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-11-19 21:33:06 -0500

    troubleshooting errors on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigation } from './components/navigation'
 import { Home } from './components/home'
 import { About } from './components/about'
 import { Portfolio } from './components/portfolio'
 import { Contact } from './components/contact'
 import { Footer } from './components/footer'
+import JsonData from "./data/data.json";
 
-export function App() {
+export function App()  
+
+ { const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []); 
+
+  {function JsonData(JsonData: any) {
+    throw new Error('Function not implemented.')
+  }
   return (
     <div>
       <Navigation />
       <Home />
       <About />
       <Portfolio />
-      <Contact />
-      <Footer />
+      <Contact data={landingPageData.Contact} />
+      <Footer data={landingPageData.Footer} />
     </div>
-  )
-}
+  );
+  }
