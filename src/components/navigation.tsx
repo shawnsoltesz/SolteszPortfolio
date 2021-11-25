363d92ba1f4cbf367c4b4587b7d1350acc452ea0
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-11-24 19:18:42 -0500

    added useEffect and UseState along with CSS

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,57 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../images/shawnsoltesz_sm_logo.png'
 
 export function Navigation() {
+  const [toggleNav, setToggleNav] = useState(false)
+
+  const [checkWidth, setCheckWidth] = useState(window.innerWidth)
+
+  const checkFunc = () => {
+    console.log(checkWidth)
+    setCheckWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkFunc)
+
+    return () => {
+      window.removeEventListener('resize', checkFunc)
+    }
+  }, [])
+
+  const toggleNavFunc = () => {
+    setToggleNav(!toggleNav)
+  }
+
   return (
     <header>
-      <div className="nav">
-        <ul className="nav-left">
-          <li>
-            <a href="#home">
-              <img src={logo} alt="Shawn Soltesz logo" width="200" />
-            </a>
-          </li>
-        </ul>
+      <>
+        <div className="nav">
+          <ul className="nav-left">
+            <li>
+              <a href="#home">
+                <img src={logo} alt="Shawn Soltesz logo" width="200" />
+              </a>
+            </li>
+          </ul>
+          {checkWidth < 900 && (
+            <button onClick={toggleNavFunc} className="floating-btn">
+              <i className="fas fa-bars"></i>
+            </button>
+          )}
 
-        <ul className="nav-right">
-          <li>
+          <nav className={toggleNav ? 'active' : ''}>
+            {checkWidth < 900 && (
+              <button onClick={toggleNavFunc} className="close-curtain">
+                X
+              </button>
+            )}
+
             <a href="#home">
-              <i className="nav-icon fas fa-home"></i>
+              <i className="nav-icon fas fa-home"></i>&nbsp;Home
             </a>
-            &nbsp;
-            <a className="nav-text" href="#home">
-              Home
-            </a>
-          </li>
-          <li>
             <a href="#about">
-              <i className="nav-icon fas fa-info-circle"></i>
+              <i className="nav-icon fas fa-info-circle"></i>&nbsp;About
             </a>
-            &nbsp;
-            <a className="nav-text" href="#about">
-              About
-            </a>
-          </li>
-          <li>
             <a href="#portfolio">
-              <i className="nav-icon fas fa-briefcase"></i>
+              <i className="nav-icon fas fa-briefcase"></i>&nbsp;Portfolio
             </a>
-            &nbsp;
-            <a className="nav-text" href="#portfolio">
-              Portfolio
-            </a>
-          </li>
-          <li>
             <a href="#contact">
-              <i className="nav-icon fas fa-mail-bulk"></i>
+              <i className="nav-icon fas fa-mail-bulk"></i>&nbsp;Contact
             </a>
-            &nbsp;
-            <a className="nav-text" href="#contact">
-              &nbsp;Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+          </nav>
+
+          <ul className="nav-right">
+            <li>
+              <a href="#home">
+                <i className="nav-icon fas fa-home"></i>
+              </a>
+              &nbsp;
+              <a className="nav-text" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                <i className="nav-icon fas fa-info-circle"></i>
+              </a>
+              &nbsp;
+              <a className="nav-text" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio">
+                <i className="nav-icon fas fa-briefcase"></i>
+              </a>
+              &nbsp;
+              <a className="nav-text" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                <i className="nav-icon fas fa-mail-bulk"></i>
+              </a>
+              &nbsp;
+              <a className="nav-text" href="#contact">
+                &nbsp;Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
     </header>
   )
 }
